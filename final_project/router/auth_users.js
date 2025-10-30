e42a0330c43b31a54712cb05198039086ed5d0f6
rodrigcasio <rod.casio@outlook.com>
2025-10-30 17:00:59 -0600

    implemented function helper `isValid` to add logic and functionality to `/register` endpoint, to register public users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,36 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+  let userWithSameName = users.filter((user) => {
+    return user.userName === username;
+  });
+
+  if (userWithSameName.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  const validUser = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+
+  if (validUser.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,14 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  if (username && password) {
+    if (!isValid(username)) {
+       users.push({ "username": username, "password": password });
+    }
+  }
 });
 
 // Get the book list available in the shop
