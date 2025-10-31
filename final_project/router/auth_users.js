881b45d3153f6adc1ac1089123cfa3076ae35d48
rodrigcasio <rod.casio@outlook.com>
2025-10-31 17:38:16 -0600

    Added functionality for endpoint `/customer/auth/review/:isbn` ready for testing PUT HTTP method

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -52,6 +52,7 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
   const book = books[isbn];
 
   if (!book) {
@@ -65,7 +66,7 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(400).json({ message: 'Missing review. Please place a review' });
   }
 
-  book.reviews[{ username: review }];
+  book.reviews = { user: username, review: review };
 
   res.status(200).json({
     user: username,
