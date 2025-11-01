c79fbc22d4d155d842f4ff04f66e1ea1e07bd43e
rodrigcasio <rod.casio@outlook.com>
2025-10-31 20:41:44 -0600

    Modified `/auth/review/:isbn` endpoint

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -86,9 +86,10 @@ regd_users.delete('/auth/review/:isbn', (req, res) => {   // 10.
 
   let reviews = book.reviews;
   let username = req.session.authorization.username;
+  let userReview = reviews[username];
 
-  if (reviews["username"]) {
-    delete reviews["username"];
+  if (userReview) {
+    delete books.reviews.username;
     
     return res.status(200).json({ message: `Review for book '${book.title}' has been deleted successfully` });
   } else {
